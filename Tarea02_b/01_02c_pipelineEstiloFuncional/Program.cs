ba72d40ec6c7f2805872b9b9ecfaba0a09f97257
ElPiche <lucasfidel85@gmail.com>
2024-08-26 19:20:13 -0300

    Tarea-02_B Refactorizar ejemplo planteado en clases

---
+++ Tarea02_b/01_02c_pipelineEstiloFuncional/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _01_02c_pipelineEstiloFuncional;
+using _01_02c_pipelineEstiloFuncional.pipeline;
+
+//new EjemploEstiloFuncional().probar();
+
+
+PipeLineEstiloFuncional pipeLine= new PipeLineEstiloFuncional();
+
+/*
+pipeLine.AgregarEndPoint(new Endpoint("/casoA", (string s) => {
+    Console.WriteLine("Haciendo algo interesante al recibir request casoA trabajando con List");
+    Console.WriteLine($"Parámetro recibido: {s}");
+}));
+
+
+pipeLine.AgregarEndPoint(new Endpoint("/casoB", (string s) => {
+    Console.WriteLine("Haciendo algo interesante al recibir request casoB trabajando con List");
+}));
+
+pipeLine.AgregarEndPoint(new Endpoint("/casoC", (string s) => {
+    Console.WriteLine("Haciendo algo interesante al recibir request casoC trabajando con List");
+}));
+
+// cada vez que recibo un request ejecuto el pipeLine pasando como parámetro
+// la urlDestino
+pipeLine.ProcesarRequestEntrante("/casoB");
+*/
+
+// Diseño utilizando map.
+
+pipeLine.AgregarEndPointMap(new Endpoint("/casoA", (string s) => {
+    Console.WriteLine("Haciendo algo interesante al recibir request casoA trabajando con MAP");
+    Console.WriteLine($"Parámetro recibido: {s}");
+}));
+
+pipeLine.AgregarEndPointMap(new Endpoint("/casoB", (string s) => {
+    Console.WriteLine("Haciendo algo interesante al recibir request casoB trabajando con MAP");
+    Console.WriteLine($"Parámetro recibido: {s}");
+}));
+
+pipeLine.AgregarEndPointMap(new Endpoint("/casoC", (string s) => {
+    Console.WriteLine("Haciendo algo interesante al recibir request casoC trabajando con MAP");
+    Console.WriteLine($"Parámetro recibido: {s}");
+}));
+
+pipeLine.ProcesarRequestEntranteMap("/casoB");
+
+
+
---
+++ Tarea02_b/01_02c_pipelineEstiloFuncional/pipeline/PipeLineEstiloFuncional.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_02c_pipelineEstiloFuncional.pipeline
+{
+
+    internal class PipeLineEstiloFuncional
+    {
+        //notese que por simplicidad aqui no estoy utilizando una interface
+        //private IList<Endpoint> _endpoints; //lista de endpoints orden O(n)
+
+        private Dictionary<string, Endpoint> _endpointsMap; //map (dictionary) de endpoints con orden de ejecución de O(1), en teoria mas rapido a la hora de buscar.
+
+        public PipeLineEstiloFuncional()
+        {
+            //_endpoints = new List<Endpoint>();
+            _endpointsMap = new Dictionary<string , Endpoint>();
+        }
+        /*
+        public void AgregarEndPoint(Endpoint nuevoEndpoint)
+        {
+            _endpoints.Add(nuevoEndpoint);
+        }
+        */
+        public void AgregarEndPointMap(Endpoint nuevoEndpoint)
+        {
+
+            string url = nuevoEndpoint.GetUrl(); //será la clave de mi objeto endpoint
+
+            if (!_endpointsMap.ContainsKey(url))
+            {
+                _endpointsMap.Add(url, nuevoEndpoint);
+            }
+            else
+            {
+                Console.WriteLine("El endpoint ya se encuentra registrado");
+            }
+        }
+
+        /*
+        public void ProcesarRequestEntrante(string urlRequestEntrate)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            foreach (Endpoint nuevoEndpoint in _endpoints)
+            {
+                nuevoEndpoint.Evaluar(urlRequestEntrate);
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine("Tiempo medido en milisegundos: " + stopwatch.ElapsedMilliseconds.ToString());
+            Console.WriteLine("Tiempo medido en ticks: " + stopwatch.ElapsedTicks.ToString());
+        }
+        */
+        public void ProcesarRequestEntranteMap(string urlRequestEntrate)
+        {
+            Stopwatch stopwatch = new Stopwatch(); 
+            stopwatch.Start();
+
+            if (_endpointsMap.TryGetValue(urlRequestEntrate, out Endpoint endPoint))
+            {
+                endPoint.Evaluar(urlRequestEntrate);
+
+                stopwatch.Stop();
+
+                Console.WriteLine("Tiempo medido en milisegundos: " + stopwatch.ElapsedMilliseconds.ToString()); //imprimo tiempo de demora de la busqueda.
+                Console.WriteLine("Tiempo medido en ticks: " + stopwatch.ElapsedTicks.ToString());
+            }
+            else
+            {
+                Console.WriteLine("No existe endpoint.");
+            }
+
+        }
+    }
+}
